d90095797f4f8ccb695bcf90ce25942d3fe1121c
Panayiotis Lipiridis <lipiridis@gmail.com>
2015-11-30 11:54:15 +0100

    Native notifications

--- electron/script.js
+++ electron/script.js
@@ -3,10 +3,25 @@
 let ipc = require('electron').ipcRenderer;
 
 var button = document.getElementById('notify');
+var Notification = window.Notification;
 
 button.addEventListener('click', function(event) {
-  ipc.send('notify', {
-    'title': 'Hello, World!',
-    'message': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
-  });
+  var instance = new Notification(
+    'Hello, World!', {
+      body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+    }
+  );
+
+  instance.onclick = function () {
+    // Something to do
+  };
+  instance.onerror = function () {
+    // Something to do
+  };
+  instance.onshow = function () {
+    // Something to do
+  };
+  instance.onclose = function () {
+    // Something to do
+  };
 });
