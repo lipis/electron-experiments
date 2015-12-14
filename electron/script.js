d16d2456c3af3ffd75c1d71f3b2eeb083200a7fd
Panayiotis Lipiridis <lipiridis@gmail.com>
2015-12-14 12:48:08 +0100

    Updates

--- electron/script.js
+++ electron/script.js
@@ -9,20 +9,7 @@ button.addEventListener('click', function(event) {
   var instance = new window.Notification(
     'Hello, World!', {
       body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
-      icon: 'https://dl.dropboxusercontent.com/u/15234/wire/wire.png'
+      icon: 'https://dl.dropboxusercontent.com/u/15234/gravatar.jpg'
     }
   );
-
-  instance.onclick = function () {
-    // Something to do
-  };
-  instance.onerror = function () {
-    // Something to do
-  };
-  instance.onshow = function () {
-    // Something to do
-  };
-  instance.onclose = function () {
-    // Something to do
-  };
 });
